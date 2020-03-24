80afee90fe813e40722814d5ae86e3ead6bcc4fc
chiefsmurph <chiefsmurph@gmail.com>
2020-03-24 07:30:02 -0700

    nah

--- realtime/collections/get-jimmy-collection.js
+++ realtime/collections/get-jimmy-collection.js
@@ -2,12 +2,13 @@ const { pick } = require('underscore');
 const isJimmyPick = require('../../utils/is-jimmy-pick');
 
 module.exports = async baseCollections => {
-  strlog({ baseCollections });
+  console.log('getting jimmy collection based on base base collections...')
+  // strlog({ baseCollections });
   const ofInterest = Object.values(baseCollections).flatten().filter(({ ticker, quote }) => !quote || quote.currentPrice < 1.5);
-  strlog({ ofInterest })
+  // strlog({ ofInterest })
   const analyzed = await isJimmyPick(
     ofInterest.map(({ ticker }) => ticker)
   );
-  strlog({ analyzed })
+  // strlog({ analyzed })
   return analyzed.filter(({ isJimmyPick }) => isJimmyPick);
 };
