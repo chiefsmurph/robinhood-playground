1777f8189b952a67b69671f0d56dcd4459f5c2df
chiefsmurph <chiefsmurph@gmail.com>
2020-03-29 15:41:18 -0700

    party

--- client/src/pages/Logs.js
+++ client/src/pages/Logs.js
@@ -3,15 +3,15 @@ class Logs extends Component {
     render() {
         console.log(this.props.mostRecentLogs);
         return (
-            <div style={{ padding: '1em'}}>
+            <div style={{ padding: '1em', font: 'monospace', backgroundColor: 'black' }}>
                 {
                     this.props.mostRecentLogs.slice(0, 30).map(log => (
-                        <pre>
-                            {
+                        <div style={{ color: JSON.stringify(log).toLowerCase().includes('error') ? 'red' : 'white', borderBottom: '1px solid white' }}>
+                            <b>{
                                 (new Date(log.timestamp)).toLocaleString()
-                            }:&nbsp;&nbsp;
+                            }:&nbsp;</b>
                             {log.title}
-                        </pre>
+                        </div>
                     ))
                 }
             </div>
