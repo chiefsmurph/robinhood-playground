a7c05627cecaf3b7a052da7b401988a1d58b5b26
chiefsmurph <chiefsmurph@gmail.com>
2020-04-02 10:53:49 -0700

    try this

--- utils/PositionWatcher.js
+++ utils/PositionWatcher.js
@@ -127,7 +127,7 @@ module.exports = class PositionWatcher {
 
     const totalNum = numAvgDowners + daysOld + mostRecentPurchase;
     let shouldAvgDownWhen = [
-      -3.5 - totalNum * 1,    // fillPickLimit
+      -3.5,    // fillPickLimit
       -3 - totalNum * 1.2     // returnLimit
     ];
     
