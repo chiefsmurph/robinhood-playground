820f82c625db289dc7cd0a2f9ab51bef187ac2ae
chiefsmurph <chiefsmurph@gmail.com>
2020-04-06 06:44:02 -0700

    f

--- app-actions/record-picks.js
+++ app-actions/record-picks.js
@@ -125,7 +125,7 @@ const handlePick = async (strategy, min, withPrices, { keys, data }) => {
         
         if (onlyAvgDownOpenPositions && !strategy.includes('avg-downer')) {
             const { positions } = require('../socket-server/strat-manager');
-            const openPosition = (positions.alpaca || []).find(pos => pos.ticker === ticker);
+            const openPosition = (positions.alpaca || []).find(pos => pos.ticker === stocksToBuy[0]);
             if (openPosition) {
                 await log(`WARNING: UNRECOMMENDING ${ticker} because this ain't an avg-downer and we already have a position open.  LETS let avg-downing take it from here, ok bucko!`)
                 isRecommended = false;
