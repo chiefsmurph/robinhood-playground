50d8361b5119087cea11a633631dd7caafa3b283
chiefsmurph <chiefsmurph@gmail.com>
2020-05-08 11:29:14 -0700

    yep

--- settings.js
+++ settings.js
@@ -51,7 +51,7 @@ module.exports = {
     ],
     
     // sellAllStocksOnNthDay: 8,
-    purchaseAmt: 1.2,
+    purchaseAmt: 2,
     onlyAvgDownOpenPositions: false,
     makeKeeperFundsAvailable: false,
     makeFundsOnlyForDowners: true,
