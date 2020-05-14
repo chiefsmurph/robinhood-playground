3c87b869f7bf5e9560540f37a53eb21d5ebcbe2b
chiefsmurph <chiefsmurph@gmail.com>
2020-05-14 06:36:15 -0700

    fix

--- utils/get-spy-trend.js
+++ utils/get-spy-trend.js
@@ -4,7 +4,7 @@ const getTrend = require('./get-trend');
 
 module.exports = cacheThis(async () => {
   const { prevClose, currentPrice } = await lookup('SPY');
-  const offset = getTrend(currentPrice, prevClose);
+  const trend = getTrend(currentPrice, prevClose);
   await log(`SPY trend set to ${trend}`)
   return trend;
 }, 20);
