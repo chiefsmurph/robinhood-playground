cd307024bcc6243fb2f1ffbb5a2f5d0b6debd4fb
chiefsmurph <chiefsmurph@gmail.com>
2020-05-15 05:51:40 -0700

    fix

--- utils/get-spy-trend.js
+++ utils/get-spy-trend.js
@@ -1,9 +1,15 @@
 const lookup = require('./lookup');
 const cacheThis = require('./cache-this');
 const getTrend = require('./get-trend');
+const {
+  getDailyHistoricals,
+} = require('../realtime/historicals/add-daily-historicals');
 
 module.exports = cacheThis(async () => {
-  const { prevClose, currentPrice } = await lookup('SPY');
+  const { SPY: daily } = await getDailyHistoricals(['SPY']);
+  const prevClose = daily[daily.length - 1].close_price;
+  const { currentPrice } = await lookup('SPY');
+  console.log({ prevClose, currentPrice })
   const trend = getTrend(currentPrice, prevClose);
   await log(`SPY trend set to ${trend}`)
   return trend;
