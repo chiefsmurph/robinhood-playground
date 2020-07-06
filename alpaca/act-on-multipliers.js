e8ce67ee9274bbd9d6886608a063ff7848acb18b
chiefsmurph <chiefsmurph@gmail.com>
2020-07-06 10:42:15 -0700

    hey

--- alpaca/act-on-multipliers.js
+++ alpaca/act-on-multipliers.js
@@ -11,7 +11,7 @@ const getMinutesFromOpen = require('../utils/get-minutes-from-open');
 module.exports = async () => {
 
   const account = await alpaca.getAccount();
-  let amtToSpend = Number(account.equity * actOnStPercent / 100) / 1.5;
+  let amtToSpend = Number(account.equity * actOnStPercent / 100);
 
   if (onlyUseCash) {
     amtToSpend *= 0.6;
