9c218df2b979401cd9e39fb9107478f5577ec2a8
chiefsmurph <chiefsmurph@gmail.com>
2020-08-25 06:45:03 -0700

    aight

--- alpaca/index.js
+++ alpaca/index.js
@@ -24,30 +24,30 @@ updates_client.connect()
 
 
 // data client updates
-const client = alpaca.data_ws
-client.onConnect(function() {
-  console.log("Connected")
-  client.subscribe(['alpacadatav1/T.FB', 'Q.AAPL', 'A.FB', 'AM.AAPL'])
-})
-client.onDisconnect(() => {
-  console.log("Disconnected")
-})
-client.onStateChange(newState => {
-  console.log(`State changed to ${newState}`)
-})
-client.onStockTrades(function(subject, data) {
-  console.log(`Stock trades: ${subject}, price: ${data.price}`)
-})
-client.onStockQuotes(function(subject, data) {
-  console.log(`Stock quotes: ${subject}, bid: ${data.bidprice}, ask: ${data.askprice}`)
-})
-client.onStockAggSec(function(subject, data) {
-  console.log(`Stock agg sec: ${subject}, ${data}`)
-})
-client.onStockAggMin(function(subject, data) {
-  console.log(`Stock agg min: ${subject}, ${data}`)
-})
-client.connect()
+// const client = alpaca.data_ws
+// client.onConnect(function() {
+//   console.log("Connected")
+//   client.subscribe(['alpacadatav1/T.FB', 'Q.AAPL', 'A.FB', 'AM.AAPL'])
+// })
+// client.onDisconnect(() => {
+//   console.log("Disconnected")
+// })
+// client.onStateChange(newState => {
+//   console.log(`State changed to ${newState}`)
+// })
+// client.onStockTrades(function(subject, data) {
+//   console.log(`Stock trades: ${subject}, price: ${data.price}`)
+// })
+// client.onStockQuotes(function(subject, data) {
+//   console.log(`Stock quotes: ${subject}, bid: ${data.bidprice}, ask: ${data.askprice}`)
+// })
+// client.onStockAggSec(function(subject, data) {
+//   console.log(`Stock agg sec: ${subject}, ${data}`)
+// })
+// client.onStockAggMin(function(subject, data) {
+//   console.log(`Stock agg min: ${subject}, ${data}`)
+// })
+// client.connect()
 
 
 module.exports = {
