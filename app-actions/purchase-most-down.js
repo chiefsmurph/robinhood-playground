acb4a3a66c1036f7d8b48197b6e8bef10f31f7e3
chiefsmurph <chiefsmurph@gmail.com>
2020-09-04 03:54:03 -0700

    here we go

--- app-actions/purchase-most-down.js
+++ app-actions/purchase-most-down.js
@@ -5,13 +5,13 @@ const Pick = require('../models/Pick');
 
 module.exports = async () => {
 
-  const { equity } = alpaca.getBalance();
+  const { equity } = await alpaca.getBalance();
   const mostDownPick = require('../socket-server/strat-manager').getMostDownPick();
-  const ticker = mostDownPick.tickers[0];
+  const [ticker] = mostDownPick.tickers;
   await log(`purchasing most down pick - ${ticker} @ ${await lookup(ticker)}`);
   await Pick.updateOne({ _id: mostDownPick._id }, { isRecommended: true });
   await limitBuyMultiple({
-    totalAmtToSpend: equity * 0.008,
+    totalAmtToSpend: Math.ceil(equity * 0.008),
     strategy: 'most-down-pick',
     ticker
   });
