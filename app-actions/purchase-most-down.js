73673cfa1486f5832f5cd63375fee132d0b150b8
chiefsmurph <chiefsmurph@gmail.com>
2020-09-04 04:26:11 -0700

    woo hoo real excited!  programming at 4:30 in the morning

--- app-actions/purchase-most-down.js
+++ app-actions/purchase-most-down.js
@@ -7,11 +7,21 @@ module.exports = async () => {
 
   const { equity } = await alpaca.getBalance();
   const mostDownPick = require('../socket-server/strat-manager').getMostDownPick();
-  const [ticker] = mostDownPick.tickers;
-  await log(`purchasing most down pick - ${ticker} @ ${await lookup(ticker)}`);
+  if (!mostDownPick) return log('no most down pick currently');
+  const { tickers, strategy } = mostDownPick;
+  const [ticker] = tickers;
+  let totalAmtToSpend = equity * 0.007;
+  if (!strategy.includes('hotSt')) {
+    totalAmtToSpend *= 1.5;
+  }
+  if (strategy.includes('sudden')) {
+    totalAmtToSpend *= 2;
+  }
+  totalAmtToSpend = Math.ceil(totalAmtToSpend);
+  await log(`purchasing most down pick - ${ticker} ${totalAmtToSpend} @ ${await lookup(ticker)}`);
   await Pick.updateOne({ _id: mostDownPick._id }, { isRecommended: true });
   await limitBuyMultiple({
-    totalAmtToSpend: Math.ceil(equity * 0.008),
+    totalAmtToSpend,
     strategy: 'most-down-pick',
     ticker
   });
