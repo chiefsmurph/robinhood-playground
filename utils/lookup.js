df816323b77a6393b7a7cf4f9aad83a38d6723c4
chiefsmurph <chiefsmurph@gmail.com>
2020-09-24 07:19:14 -0700

    aight

--- utils/lookup.js
+++ utils/lookup.js
@@ -11,16 +11,16 @@ const { alpaca } = require('../alpaca');
 
 module.exports = async (ticker) => {
     // console.log('looking up', ticker);
-    const alpacaResponse = await alpaca.lastQuote(ticker);
-    const alpacaTrade = await alpaca.lastTrade(ticker);
+    // const alpacaResponse = await alpaca.lastQuote(ticker);
+    // const alpacaTrade = await alpaca.lastTrade(ticker);
     // strlog({ alpacaResponse})
     const quoteDataResponse = await Robinhood.quote_data(ticker);
-    strlog({
-        alpacaResponse,
-        alpacaTrade,
-        quoteDataResponse,
-        ASSET: await alpaca.getAsset(ticker)
-    })
+    // strlog({
+    //     alpacaResponse,
+    //     alpacaTrade,
+    //     quoteDataResponse,
+    //     ASSET: await alpaca.getAsset(ticker)
+    // })
     const originalQuoteData = (quoteDataResponse.results || [])[0];
     
     // add yahoo price
