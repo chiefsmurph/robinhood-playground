d0e9970300cac893ab0a25c93c94ec9d00aa16b9
chiefsmurph <chiefsmurph@gmail.com>
2020-11-30 08:30:20 -0800

    ah

--- alpaca/relay-message.js
+++ alpaca/relay-message.js
@@ -3,6 +3,7 @@ const getPositions = require('./get-positions');
 
 module.exports = async () => {
   let positions = await getPositions();
+  positions = positions.filter(position => !position.isSelling);
   const formatPrice = price => price.toFixed(price < 1 ? 4 : 2);
   const descriptionMessage = 'my location: Palm Springs, CA\nmy current stocks: ' + positions.map(({ ticker, avgEntry }) => `${ticker} @ ${formatPrice(avgEntry)}`).join(' and ');
   strlog({ positions, descriptionMessage });
