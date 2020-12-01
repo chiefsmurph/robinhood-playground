ec5b194a93eb2db65ed8b9808ebf922f4400cad7
chiefsmurph <chiefsmurph@gmail.com>
2020-11-30 20:47:18 -0800

    go

--- alpaca/relay-message.js
+++ alpaca/relay-message.js
@@ -1,12 +1,13 @@
 const setDescription = require('../insta/set-description');
 const getPositions = require('./get-positions');
 
+const roundTo = digits => n => Math.round(n * Math.pow(10, digits)) / Math.pow(10, digits); 
+const formatPrice = price => roundTo(price < 1 ? 4 : 2)(price);
+
 module.exports = async () => {
   let positions = await getPositions();
   positions = positions.filter(position => !position.isSelling);
-  const formatPrice = price => price.toFixed(price < 1 ? 4 : 2);
   // const descriptionMessage = 'my location: Palm Springs, CA\nmy current stocks: ' + 
-  const descriptionMessage = positions.map(({ ticker, avgEntry }) => `${ticker} @ ${formatPrice(avgEntry)}`).join(' and ');
-  strlog({ positions, descriptionMessage });
+  const descriptionMessage = positions.slice(0, 5).map(({ ticker, avgEntry }) => `${ticker} @ ${formatPrice(avgEntry)}`).join('\n');
   await setDescription(descriptionMessage);
 };
