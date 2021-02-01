f2871f2621b8fcdfb8c090cd7a9aeb5ce44d0313
chiefsmurph <chiefsmurph@gmail.com>
2021-01-31 18:54:55 -0600

    f

--- utils/get-recent-picks-for-ticker.js
+++ utils/get-recent-picks-for-ticker.js
@@ -1,4 +1,5 @@
 const Pick = require('../models/Pick');
+const cacheThis = require('./cache-this');
 module.exports = cacheThis(async (...params) => {
     const response = await Pick.getRecentPicksForTicker(...params);
     return response;
