ea62c6ef15f91e8aaa2fe6aeaa21f73748f0faff
chiefsmurph <chiefsmurph@gmail.com>
2021-02-10 03:12:29 -0600

    ah

--- app-actions/buys-in-progress.js
+++ app-actions/buys-in-progress.js
@@ -21,7 +21,7 @@ module.exports = {
     getActiveStrategy: ticker => {
         const curVal = buysInProgress[ticker];
         log(`${ticker} currently ${curVal}`)
-        if (!curVal || curVal.length) return;   // nothing ? return undefined
+        if (!curVal || !curVal.length) return;   // nothing ? return undefined
         const relatedPosition = getRelatedPosition(ticker);
         const rocketString = relatedPosition.stSent.wordFlags.includes('rocket') && 'rocket';
         return [
