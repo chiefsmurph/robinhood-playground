124a9835233f37e45bd13afebfb16549817b87b6
chiefsmurph <chiefsmurph@gmail.com>
2021-02-15 10:40:51 -0600

    date

--- app-actions/init-modules.js
+++ app-actions/init-modules.js
@@ -2,8 +2,17 @@ const fs = require('mz/fs');
 const path = require('path');
 const regCronIncAfterSixThirty = require('../utils/reg-cron-after-630');
 const executeStrategy = require('./execute-strategy');
+const marketClosures = require('../market-closures');
 
 let modules = [];
+
+const isMarketClosed = () => {
+    const date = new Date();
+    const dateStr = date.toLocaleDateString().split('/').join('-');
+    const day = date.getDay();
+    const isWeekend = day === 0 || day === 6;
+    return isWeekend || marketClosures.includes(dateStr);
+};
 
 const initModule = (module) => {
     const {
@@ -25,6 +34,7 @@ const initModule = (module) => {
         run,
         // run: [],
         fn: async (min) => {
+            if (isMarketClosed()) return console.log(`market closed not running ${name}`);
             return !!fn 
                 ? fn(min) 
                 : executeStrategy(trendFilter, min, 0.3, name, trendFilterKey);
