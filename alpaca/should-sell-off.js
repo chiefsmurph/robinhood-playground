a2aea86eaa3d6fe0fb875eebd891809813e66064
chiefsmurph <chiefsmurph@gmail.com>
2021-02-18 10:11:08 -0600

    aight

--- alpaca/should-sell-off.js
+++ alpaca/should-sell-off.js
@@ -4,11 +4,13 @@ module.exports = position => {
     daysOld, 
     mostRecentPurchase,
     numMultipliers,
-    stSent: { bullBearScore = 0 } = {} 
+    stSent: { bullBearScore = 0 } = {},
+    market_value
   } = position;
   // return true;  // nighttrading baby!
   const conditions = [
     daysOld > 3,
+    Number(market_value) < 35
     // mostRecentPurchase > 2,
     // numMultipliers > 150,
     // bullBearScore < 100,
