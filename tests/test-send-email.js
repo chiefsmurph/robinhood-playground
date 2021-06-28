5d07e4c221b1269231b500a40050aa7f5dc382ae
chiefsmurph <chiefsmurph@gmail.com>
2021-06-27 21:35:46 -0400

    yep

--- tests/test-send-email.js
+++ tests/test-send-email.js
@@ -1,4 +1,6 @@
 const sendEmail = require('../utils/send-email');
+const { emails } = require('../config');
+
 module.exports = async () => {
-  await sendEmail('testing', 'body');
+  await sendEmail('force', 'test send email to cell', 'log string', Object.keys(emails)[1]); // cell phone
 }
